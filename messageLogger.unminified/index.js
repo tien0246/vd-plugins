6143224917a1778288b70d3e991ec494ab782fa3
tien0246 <tien0246@users.noreply.github.com>
2025-09-24 08:26:42 +0000

    deploy: adcc03091b042711c487277b15ff27b72c35c913

--- messageLogger.unminified/index.js
+++ messageLogger.unminified/index.js
@@ -1,26 +1,51 @@
-(function(exports,common,metro,toasts,_vendetta,patcher){'use strict';let unpatch;
+(function(exports,common,toasts,_vendetta){'use strict';const messageCache = /* @__PURE__ */ new Map();
+const CACHE_EXPIRY_MS = 15 * 60 * 1e3;
+const patches = [];
+function cacheMessage(message) {
+  if (!message?.id || !message.content || message.author?.bot)
+    return;
+  if (messageCache.has(message.id)) {
+    const existing = messageCache.get(message.id);
+    clearTimeout(existing.timer);
+  }
+  const timer = setTimeout(function() {
+    messageCache.delete(message.id);
+  }, CACHE_EXPIRY_MS);
+  messageCache.set(message.id, {
+    content: message.content,
+    author: message.author?.username ?? "unknown",
+    timer
+  });
+}
 var index = {
   onLoad: function() {
-    toasts.showToast("MessageLogger: onLoad called (v5)");
-    const MessageStore = metro.findByStoreName("MessageStore");
-    if (!MessageStore) {
-      toasts.showToast("ML Error: Could not find MessageStore!");
-      return;
-    }
-    unpatch = patcher.before("dispatch", common.FluxDispatcher, function(args) {
-      const action = args[0];
-      if (action.type !== "MESSAGE_DELETE")
-        return;
-      try {
-        toasts.showToast(JSON.stringify(action));
-      } catch (e) {
-        _vendetta.logger.error("MessageLogger: Error in MESSAGE_DELETE", e);
-        toasts.showToast(`ML Error: ${e.message}`);
+    patches.push(common.FluxDispatcher.subscribe("MESSAGE_CREATE", function({ message }) {
+      cacheMessage(message);
+    }));
+    patches.push(common.FluxDispatcher.subscribe("MESSAGE_UPDATE", function({ message }) {
+      if (message.content) {
+        cacheMessage(message);
       }
-    });
+    }));
+    patches.push(common.FluxDispatcher.subscribe("MESSAGE_DELETE", function(action) {
+      if (messageCache.has(action.id)) {
+        const cachedMessage = messageCache.get(action.id);
+        toasts.showToast(`Deleted from ${cachedMessage.author}: ${cachedMessage.content}`);
+        clearTimeout(cachedMessage.timer);
+        messageCache.delete(action.id);
+      }
+    }));
+    _vendetta.logger.log("MessageLogger loaded with caching strategy.");
   },
   onUnload: function() {
-    unpatch?.();
-    _vendetta.logger.log("MessageLogger unloaded.");
+    for (const unpatch of patches) {
+      unpatch?.();
+    }
+    patches.length = 0;
+    for (const [_id, cachedMessage] of messageCache) {
+      clearTimeout(cachedMessage.timer);
+    }
+    messageCache.clear();
+    _vendetta.logger.log("MessageLogger unloaded and cache cleared.");
   }
-};exports.default=index;Object.defineProperty(exports,'__esModule',{value:true});return exports;})({},vendetta.metro.common,vendetta.metro,vendetta.ui.toasts,vendetta,vendetta.patcher);
+};exports.default=index;Object.defineProperty(exports,'__esModule',{value:true});return exports;})({},vendetta.metro.common,vendetta.ui.toasts,vendetta);
