50811e0491694eb2c3638d225cf154603de67f3a
tien0246 <doantien541@gmail.com>
2025-09-24 14:11:36 +0700

    Update index.ts

--- plugins/hidden-channels/src/index.ts
+++ plugins/hidden-channels/src/index.ts
@@ -1,62 +1,73 @@
 import { findByName, findByProps } from "@vendetta/metro";
 import { constants, React } from "@vendetta/metro/common";
 import { instead, after } from "@vendetta/patcher";
+import { logger } from "@vendetta";
 import HiddenChannel from "./HiddenChannel.jsx";
 
 let patches = [];
 
-const Permissions = findByProps("getChannelPermissions", "can");
-const Router = findByProps("transitionToGuild");
-const Fetcher = findByProps("stores", "fetchMessages");
-const { ChannelTypes } = findByProps("ChannelTypes");
-const {getChannel} = findByProps("getChannel");
-
-const skipChannels = [
-    ChannelTypes.DM, 
-    ChannelTypes.GROUP_DM, 
-    ChannelTypes.GUILD_CATEGORY
-]
+const getChannel = findByProps("getChannel")?.getChannel;
+const ChannelTypes = findByProps("ChannelTypes")?.ChannelTypes;
 
 function isHidden(channel: any | undefined) {
+    if (!getChannel || !ChannelTypes) return false;
     if (channel == undefined) return false;
     if (typeof channel === 'string')
         channel = getChannel(channel);
-    if (!channel || skipChannels.includes(channel.type)) return false;
+    if (!channel || [ChannelTypes.DM, ChannelTypes.GROUP_DM, ChannelTypes.GUILD_CATEGORY].includes(channel.type)) return false;
+    
+    const Permissions = findByProps("getChannelPermissions", "can");
+    if (!Permissions) return false;
+
     channel.realCheck = true;
     let res = !Permissions.can(constants.Permissions.VIEW_CHANNEL, channel);
     delete channel.realCheck;
     return res;
 }
+
 function onLoad() {
-    const MessagesConnected = findByName("MessagesWrapperConnected", false);
-    
-    patches.push(after("can", Permissions, ([permID, channel], res) => {
-        if (!channel?.realCheck && permID === constants.Permissions.VIEW_CHANNEL) return true;
-        return res;
-    }));
+    try {
+        const Permissions = findByProps("getChannelPermissions", "can");
+        if (Permissions) {
+            patches.push(after("can", Permissions, ([permID, channel], res) => {
+                if (!channel?.realCheck && permID === constants.Permissions.VIEW_CHANNEL) return true;
+                return res;
+            }));
+        } else {
+            logger.error("Could not find Permissions module for hidden-channels.");
+        }
 
-    patches.push(instead("transitionToGuild", Router, (args, orig) => {
-        const [_, channel] = args;
-        if (!isHidden(channel) && typeof orig === "function") orig(args);
-    }));
+        const Router = findByProps("transitionToGuild");
+        if (Router) {
+            patches.push(instead("transitionToGuild", Router, (args, orig) => {
+                const [_, channel] = args;
+                if (!isHidden(channel) && typeof orig === "function") orig(...args);
+            }));
+        } else {
+            logger.error("Could not find Router module for hidden-channels.");
+        }
 
-    patches.push(instead("fetchMessages", Fetcher, (args, orig) => {
-        const [channel] = args;
-        if (!isHidden(channel) && typeof orig === "function") orig(args);
-    }));
+        const Fetcher = findByProps("stores", "fetchMessages");
+        if (Fetcher) {
+            patches.push(instead("fetchMessages", Fetcher, (args, orig) => {
+                const [channelId] = args;
+                if (!isHidden(channelId) && typeof orig === "function") orig(...args);
+            }));
+        } else {
+            logger.error("Could not find Fetcher module for hidden-channels.");
+        }
 
-    patches.push(instead("default", MessagesConnected, (args, orig) => {
-        const channel = args[0]?.channel;
-        if (!isHidden(channel) && typeof orig === "function") return orig(...args);
-        else return React.createElement(HiddenChannel, {channel});
-    }));
-}
-
-export default {
-    onLoad,
-    onUnload: () => {
-        for (const unpatch of patches) {
-            unpatch();
-        };
+        const MessagesConnected = findByName("MessagesWrapperConnected", false);
+        if (MessagesConnected) {
+            patches.push(instead("default", MessagesConnected, (args, orig) => {
+                const channel = args[0]?.channel;
+                if (!isHidden(channel) && typeof orig === "function") return orig(...args);
+                else return React.createElement(HiddenChannel, {channel});
+            }));
+        } else {
+            logger.error("Could not find MessagesConnected component for hidden-channels.");
+        }
+    } catch (e) {
+        logger.error("Failed to load hidden-channels plugin", e);
     }
 }
