77431ae73afa513885a8db2a787ed9e376c18199
tien0246 <tien0246@users.noreply.github.com>
2025-09-24 09:50:56 +0000

    deploy: 71141c6ceb473580c261663e46c11f1d646897ac

--- messageLogger.unminified/index.js
+++ messageLogger.unminified/index.js
@@ -1,4 +1,4 @@
-(function(exports,common,metro,_vendetta,plugin,patcher,assets,components,ui){'use strict';const { ScrollView, View, Text } = components.General;
+(function(exports,common,metro,toasts,_vendetta,plugin,patcher,utils,assets,components,ui){'use strict';const { ScrollView, View, Text } = components.General;
 const { FormRow, FormDivider } = components.Forms;
 const ChannelStore$1 = metro.findByStoreName("ChannelStore");
 const styles = common.stylesheet.createThemedStyleSheet({
@@ -130,10 +130,19 @@ var index = {
         const hasDeleted = plugin.storage.deletedMessages[channelId]?.length > 0;
         if (!hasDeleted)
           return;
-        const originalChildren = res?.props?.children;
-        if (!originalChildren)
+        const toolbar = utils.findInReactTree(res, function(r) {
+          return r?.type?.name === "Toolbar";
+        });
+        if (!toolbar) {
+          toasts.showToast("ML: FAILED to find Toolbar");
           return;
-        const trashButton = /* @__PURE__ */ common.React.createElement(TouchableOpacity, {
+        }
+        toasts.showToast("ML: Found Toolbar");
+        if (!Array.isArray(toolbar.props.children)) {
+          toasts.showToast("ML: Toolbar children not an array");
+          return;
+        }
+        toolbar.props.children.push(/* @__PURE__ */ common.React.createElement(TouchableOpacity, {
           onPress: function() {
             Navigation.push("VendettaCustomPage", {
               title: `Deleted Msgs in #${channel.name}`,
@@ -143,17 +152,11 @@ var index = {
                 });
               }
             });
-          },
-          style: {
-            position: "absolute",
-            right: 12,
-            top: 12,
-            zIndex: 1
           }
         }, /* @__PURE__ */ common.React.createElement(components.Forms.FormIcon, {
           source: assets.getAssetIDByName("ic_trash_24px")
-        }));
-        res.props.children = /* @__PURE__ */ common.React.createElement(common.React.Fragment, null, originalChildren, trashButton);
+        })));
+        toasts.showToast("ML: Pushed button to Toolbar");
       }));
     } else {
       _vendetta.logger.error("MessageLogger: Could not find ChannelHeader component");
@@ -167,4 +170,4 @@ var index = {
     patches.length = 0;
     _vendetta.logger.log("MessageLogger unloaded.");
   }
-};exports.default=index;Object.defineProperty(exports,'__esModule',{value:true});return exports;})({},vendetta.metro.common,vendetta.metro,vendetta,vendetta.plugin,vendetta.patcher,vendetta.ui.assets,vendetta.ui.components,vendetta.ui);
+};exports.default=index;Object.defineProperty(exports,'__esModule',{value:true});return exports;})({},vendetta.metro.common,vendetta.metro,vendetta.ui.toasts,vendetta,vendetta.plugin,vendetta.patcher,vendetta.utils,vendetta.ui.assets,vendetta.ui.components,vendetta.ui);
