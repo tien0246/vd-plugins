172bbc9d9c869f9dd5eec6fcde9a735150e4de64
tien0246 <tien0246@users.noreply.github.com>
2025-09-24 08:03:52 +0000

    deploy: 08483a51db6a1353e5b9554b13543d1684ddf3d0

--- messageLogger.unminified/index.js
+++ messageLogger.unminified/index.js
@@ -1,18 +1,26 @@
 (function(exports,common,metro,toasts,_vendetta){'use strict';let unpatch;
 var index = {
   onLoad: function() {
-    toasts.showToast("MessageLogger: onLoad called (v1)");
+    toasts.showToast("MessageLogger: onLoad called (v2)");
     _vendetta.logger.log("MessageLogger loaded.");
     const MessageStore = metro.findByStoreName("MessageStore");
+    if (!MessageStore) {
+      toasts.showToast("ML Error: Could not find MessageStore!");
+      return;
+    }
     unpatch = common.FluxDispatcher.subscribe("MESSAGE_DELETE", function(action) {
+      toasts.showToast("ML: MESSAGE_DELETE received!");
       try {
         const message = MessageStore.getMessage(action.channelId, action.id);
         if (message && message.content) {
           const author = message.author?.username ?? "unknown";
           toasts.showToast(`Deleted from ${author}: ${message.content}`);
+        } else {
+          toasts.showToast(`ML: Deleted msg [${action.id}] not in cache or has no content.`);
         }
       } catch (e) {
         _vendetta.logger.error("MessageLogger: Error in MESSAGE_DELETE", e);
+        toasts.showToast(`ML Error: ${e.message}`);
       }
     });
   },
